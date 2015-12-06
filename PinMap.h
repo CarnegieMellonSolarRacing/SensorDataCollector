38fa5a1e6aca221975ec8d41849c99a8940d4dfb
Jon Buckley <jonbuckley33@gmail.com>
2015-12-06 16:34:56 -0500

    added comment

--- PinMap.h
+++ PinMap.h
@@ -4,5 +4,7 @@
 #include <WProgram.h>
  
 // Declare mappings of pins to I/O devices here.
+// Eg. static int BATTERY_THERMOMETER_PIN = 9;
+// Then in other code, you can simply reference this pin.
  
 #endif
