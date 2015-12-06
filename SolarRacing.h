ffc37939a0f337ed2d17f56447f6bd94e7bcb77c
Frances Tso <francest1995@hotmail.com>
2015-12-06 16:36:53 -0500

    Create SolarRacing.h

---
+++ SolarRacing.h
@@ -0,0 +1,19 @@
+#ifndef SOLAR_PANEL_H 
+#define SOLAR_PANEL_H
+ 
+#include <WProgram.h>
+ 
+class SolarPanel {
+public:
+		//constructor
+        SolarPanel();
+        ~SolarPanel();
+        // Gets the current going through the solar panel,
+        // out of 100.
+        float getCurrent();
+        // Gets the change in current from the previous time step 
+        // (most recent reading),
+        float getChangeInCurrent();
+};
+ 
+#endif
