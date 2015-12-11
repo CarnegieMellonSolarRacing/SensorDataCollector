4c9e12fdda91b892fca58044666ebb27b81a1257
jeremyahuang <jeremy@Cathys-MBP-2.wv.cc.cmu.edu>
2015-12-10 19:10:00 -0500

    changed tabs to spaces. :(

--- BatteryMeter.h
+++ BatteryMeter.h
@@ -5,12 +5,12 @@
  
 class BatteryMeter {
 public:
-	BatteryMeter();
-	~BatteryMeter();
-	// Gets the current charge of the battery,
-	// out of 100.
+    BatteryMeter();
+    ~BatteryMeter();
+    // Gets the current charge of the battery,
+    // out of 100.
     float getPercentCharge();
-	// Gets the current temperature on the battery,
+    // Gets the current temperature on the battery,
     // in celcius.
     float getTemperature();
 };
